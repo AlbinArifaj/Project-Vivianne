ce5922b65c51bd4705abb4abfde74f9fd9c7c8c1
AlbinArifaj <albiniarifaj@gmail.com>
2025-02-19 21:38:50 +0100

    Add Convert_Date_Time test

---
+++ src/test/java/ConvertDateTimeToLocalDateTimeTest.java
@@ -0,0 +1,44 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.Test;
+import service.DateConversion;
+import service.GenerateEmail;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ConvertDateTimeToLocalDateTimeTest {
+    public static final DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+    @Test
+    void validLocalDateTimeConversion(){
+        LocalDate localDate = LocalDate.now();
+        String hour ="04";
+        String minutes="23";
+        LocalDateTime localDateTime =LocalDateTime.parse(localDate+" "+hour+":"+minutes+":00",dateTimeFormatter);
+        assertEquals(localDateTime,DateConversion.fromDateTimeComponents(localDate,hour,minutes));
+    }
+    @Test
+    void invalidLocalDateTimeConversion(){
+        LocalDate localDate = LocalDate.now();
+        String hour ="4";
+        assertNull(DateConversion.fromDateTimeComponents(localDate, hour, null));
+    }
+    @Test
+    void singleDigitHourAndMinute(){
+        LocalDate localDate = LocalDate.now();
+        String hour ="04";
+        String minutes="3";
+        assertNull(DateConversion.fromDateTimeComponents(localDate, hour, minutes));
+    }
+    @Test
+    void invalidHour() {
+        LocalDate date = LocalDate.of(2025, 2, 17);
+        assertThrows(DateTimeParseException.class, () -> {
+            DateConversion.fromDateTimeComponents(date, "29", "23");
+        });
+    }
+}
